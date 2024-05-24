820b3cc5b31076e3d2e8b43992495c153855ab05
LucasMaciel.Gois-ext <LucasMaciel.Gois-ext@ab-Inbev.com>
2024-05-23 22:02:10 -0300

    Created projects

---
+++ demo-backend-api/src/main/java/com/lmg/demo_backend_api/DemoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.lmg.demo_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendApiApplication.class, args);
+	}
+
+}
